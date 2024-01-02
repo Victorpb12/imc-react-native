c121185ccb42a7c31621ec2ab550c9f3e5919afc
victorpb12 <victorpbaumg@gmail.com>
2024-01-02 14:29:18 -0300

    Adicionado API Share para compartilhar o IMC

--- src/components/Form/ResultImc/index.js
+++ src/components/Form/ResultImc/index.js
@@ -1,9 +1,30 @@
-import { View, Text } from 'react-native';
+import { 
+  View, 
+  Text,
+  TouchableOpacity,
+  Share, 
+} from 'react-native';
 import styles from './style';
 
 export default function ResultImc(props) {
+  const onShare = async () => {
+    const result = await Share.share({
+      message: 'Meu IMC hoje é: ' + props.resultImc,
+    });
+  };
+
   return (
     <View style={styles.resultImc}>
+      <View style={styles.boxShare}>
+        {props.resultImc != null ?
+        <TouchableOpacity 
+        style={styles.shared}
+        onPress={() => onShare()}>
+          <Text style={styles.sharedText}>Compartilhar</Text>
+        </TouchableOpacity>
+        : <View/>
+        } 
+      </View>
       <Text style={styles.information}>{props.messageResultImc}</Text>
       <Text style={styles.numberImc}>{props.resultImc}</Text>
     </View>
